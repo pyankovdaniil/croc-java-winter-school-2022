2bb849340b919fba0d1425c41cfe7c1c47a9f300
Pyankov Daniil <pyankovdaniil@yandex.ru>
2022-10-27 10:18:46 +0300

    Try to fix empty files

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -1,2 +1,29 @@
-package ru.croc.task2;public class Task2 {
+package ru.croc.task2;
+
+import ru.croc.task2.util.ArithmeticProgressionAccumulator;
+
+public class Task2 {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 3) {
+                throw new IllegalArgumentException("Number of arguments should be = 3");
+            }
+
+            double firstMember = Double.parseDouble(args[0]);
+            double difference = Double.parseDouble(args[1]);
+            int numberOfMembers = Integer.parseInt(args[2]);
+
+            double sum = ArithmeticProgressionAccumulator.accumulate(firstMember, difference, numberOfMembers);
+
+            String sumString = String.valueOf(sum);
+            System.out.print("Sum: ");
+            if (sumString.substring(sumString.length() - 2).equals(".0")) {
+                System.out.println(sumString.substring(0, sumString.length() - 2));
+            } else {
+                System.out.println(sum);
+            }
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
 }
